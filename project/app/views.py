eb0de4fa6afd3c07918f79907e1d0143764f7e70
orroo <omarriahi2003@gmail.com>
2024-11-20 00:01:18 +0100

    tempv0.1

---
+++ project/app/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+
+# Create your views here.
+def my_view(request):
+    
+    return render(request, 'index.html')
+
+
+def my_view_404(request):
+    
+    return render(request, '404.html')
+
+
+def my_view_about(request):
+    
+    return render(request, 'about.html')
+
+def my_view_blog(request):
+    
+    return render(request, 'blog.html')
+
+def my_view_cars(request):
+    
+    return render(request, 'cars.html')
+
+
+def my_view_contact(request):
+    
+    return render(request, 'contact.html')
+
+
+def my_view_feature(request):
+    
+    return render(request, 'feature.html')
+
+
+
+
+def my_view_service(request):
+    
+    return render(request, 'service.html')
+
+
+def my_view_team(request):
+    
+    return render(request, 'team.html')
+
+
+
+
+def my_view_testimonial(request):
+    
+    return render(request, 'testimonial.html')
