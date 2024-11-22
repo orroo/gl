70dc43f81eacd8309f304048477ca69066d926cf
amina3107 <130994369+amina3107@users.noreply.github.com>
2024-11-22 22:46:23 +0100

    partie recherche amina

--- Projet/Catalogue/views.py
+++ Projet/Catalogue/views.py
@@ -5,6 +5,8 @@ from django.views.generic import ListView, DetailView
 from rest_framework import generics
 from .serializer import OffreSerializer
 from django.core.paginator import Paginator
+from django.db.models import Q
+
 
 
 class Offredetail(generics.RetrieveUpdateDestroyAPIView):
@@ -22,30 +24,23 @@ class OffreListView(ListView):
     paginate_by = 6  # Nombre d'offres par page
     def get_queryset(self):
         queryset = Offre.objects.all()
-
-        # Recherche par mot-clé
         query = self.request.GET.get('q')
+        min_price = self.request.GET.get('min_price')
+        max_price = self.request.GET.get('max_price')
+
         if query:
             queryset = queryset.filter(
                 Q(title__icontains=query) | Q(description__icontains=query)
             )
 
-        # Filtrage par prix
-        min_price = self.request.GET.get('min_price')
-        max_price = self.request.GET.get('max_price')
         if min_price:
             queryset = queryset.filter(price__gte=min_price)
+
         if max_price:
             queryset = queryset.filter(price__lte=max_price)
 
-        # Tri des offres
-        sort_by = self.request.GET.get('sort_by')
-        if sort_by == 'price_asc':
-            queryset = queryset.order_by('price')
-        elif sort_by == 'price_desc':
-            queryset = queryset.order_by('-price')
-
         return queryset
+
 
 class OffreDetailView(DetailView):
     model = Offre
