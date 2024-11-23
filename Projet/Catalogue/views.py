c5b532e9e00a029e3279bfba8ad76828455a40db
amina3107 <130994369+amina3107@users.noreply.github.com>
2024-11-23 21:29:02 +0100

    .

--- Projet/Catalogue/views.py
+++ Projet/Catalogue/views.py
@@ -49,4 +49,47 @@ class OffreDetailView(DetailView):
     
     
 
+'''
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from .models import Offre
+from conducteurapp.models import conducteur
 
+def acheter_offre(request, offre_id):
+    # Vérifie si l'utilisateur est connecté
+    if not request.user.is_authenticated:
+        messages.error(request, "Vous devez être connecté pour acheter une offre.")
+        return redirect('login')  # Redirigez vers la page de connexion
+
+    # Assure-toi que l'utilisateur est un conducteur
+    try:
+        conducteur_instance = request.user.conducteur
+    except AttributeError:
+        messages.error(request, "Vous devez être un conducteur pour acheter une offre.")
+        return redirect('offre_list')  # Redirige vers la liste des offres
+
+    # Récupère l'offre demandée
+    offre = get_object_or_404(Offre, id=offre_id)
+
+    # Vérifie si le conducteur a déjà acheté l'offre
+    if offre in conducteur_instance.offres_achetees.all():
+        messages.error(request, "Vous avez déjà acheté cette offre.")
+        return redirect('offre_detail', offre_id=offre.id)
+
+    # Vérifie si le conducteur a suffisamment de jetons
+    if conducteur_instance.nb_jetons < offre.price:
+        messages.error(request, "Vous n'avez pas assez de jetons pour acheter cette offre.")
+        return redirect('offre_detail', offre_id=offre.id)
+
+    # Déduis le prix de l'offre des jetons du conducteur
+    conducteur_instance.nb_jetons -= offre.price
+    conducteur_instance.save()
+
+    # Ajoute l'offre à la liste des offres achetées
+    conducteur_instance.offres_achetees.add(offre)
+
+    # Message de confirmation
+    messages.success(request, f"Félicitations ! Vous avez acheté l'offre '{offre.title}'.")
+
+    return redirect('offre_detail', offre_id=offre.id)
+'''
